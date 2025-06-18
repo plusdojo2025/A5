e8bf6ec23e929ee2070969c8b8823094da2690e4
plusdojo2025-KAWASAKI <kawasaki-haruna-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-18 16:25:59 +0900

    途中

--- source/src/main/java/servlet/UserRegistServlet.java
+++ source/src/main/java/servlet/UserRegistServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,30 +13,30 @@ import javax.servlet.http.HttpSession;
 
 import dao.BcDAO;
 import dto.Bc;
-import dto.Result;
 
 /**
  * Servlet implementation class RegistServlet
  */
-@WebServlet("/RegistServlet")
-public class RegistServlet extends HttpServlet {
+@WebServlet("/UserRegistServlet")
+public class UserRegistServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
+	//画面表示：ユーザー登録画面へ
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-		HttpSession session = request.getSession();
-		if (session.getAttribute("id") == null) {
-			response.sendRedirect("/webapp/LoginServlet");
-			return;
-		}
+		// ログインしていなかったらログインサーブレットにリダイレクトする（ログイン画面に戻る）
+//		HttpSession session = request.getSession();
+//		if (session.getAttribute("id") == null) {
+//			response.sendRedirect(request.getContextPath() + "/LoginServlet");
+//			return;
+//		}
 
 		// 登録ページにフォワードする
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/regist.jsp");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/tencho_user_reg.jsp");
 		dispatcher.forward(request, response);
 	}
 
@@ -54,25 +55,27 @@ public class RegistServlet extends HttpServlet {
 
 		// リクエストパラメータを取得する
 		request.setCharacterEncoding("UTF-8");
-		String company = request.getParameter("company");
-		String department = request.getParameter("department");
-		String position = request.getParameter("position");
+		String role = request.getParameter("employees");
 		String name = request.getParameter("name");
-		String phone = request.getParameter("phone");
-		String email = request.getParameter("email");
-		String zipcode = request.getParameter("zipcode");
-		String address = request.getParameter("address");
+		String password = request.getParameter("password");
+		
+		//リクエストパラメータのチェック
+		if(role.equals("0")) {role="店長";}
+		else if(role.equals("1")) {role="店員";}
 
 		// 登録処理を行う
-		BcDAO bDao = new BcDAO();
-		if (bDao.insert(new Bc(0, company,department,position ,name,phone,email, zipcode,address))) { // 登録成功
-			request.setAttribute("result", new Result("登録が成功しました", "正常に動作しました", "/webapp/MenuServlet"));
-		} else { // 登録失敗
-			request.setAttribute("result", new Result("登録に失敗しました", "正常に動作しませんでした", "/webapp/MenuServlet"));
+				BcDAO bDao = new BcDAO(); 
+				Bc newUser =new Bc(role,name,password);
+				List<Bc> userList = bDao.select(newUser); //↓短縮バージョン
+
+				//List<Bc> cardList = bDao.select(new Bc(0, company,department,position ,name,phone,email, zipcode,address));
+
+				// 登録結果をリクエストスコープに格納する
+				request.setAttribute("userList", userList);
+
+				// 結果ページにフォワードする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/tencho_user_edit.jsp");
+				dispatcher.forward(request, response);
+			}
 		}
 
-		// 結果ページにフォワードする
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
-		dispatcher.forward(request, response);
-	}
-}
