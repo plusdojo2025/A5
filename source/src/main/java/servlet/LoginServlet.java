2cdf016f1817a403abdb8dd4fd4b84995eb8ca66
plusdojo2025-futagami <futagami-masanobu-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-18 14:48:36 +0900

    Create LoginServlet.java

---
+++ source/src/main/java/servlet/LoginServlet.java
@@ -0,0 +1,76 @@
+package servlet;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.IdPwDAO;
+import dto.IdPw;
+import dto.LoginUser;
+import dto.Result;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// ログインページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+
+		// ログイン処理を行う
+		IdPwDAO iDao = new IdPwDAO();
+		String user = iDao.isLoginOK(new IdPw(id, pw));
+		if (user != null) { // ログイン成功
+			// セッションスコープに名前を格納する
+			HttpSession session = request.getSession();
+			LoginUser loginuser = new LoginUser(user);
+			session.setAttribute("user", loginuser.getName());
+			
+			// 日時を取得しセッションスコープに格納する
+			Date now = new Date();
+			SimpleDateFormat date = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+			String formatDate = date.format(now);
+			session.setAttribute("date", formatDate);
+
+			// メニューサーブレットにリダイレクトする
+			response.sendRedirect("/webapp/MenuServlet");
+		} else { // ログイン失敗
+			// リクエストスコープに、タイトル、メッセージ、戻り先を格納する
+			request.setAttribute("result", new Result("ログイン失敗！", "IDまたはPWに間違いがあります。", "/webapp/LoginServlet"));
+
+			// 結果ページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
+}
