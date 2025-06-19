c1c13f94ee7604917182fcb631e546287f3e326b
plusdojo2025-futagami <futagami-masanobu-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-19 11:45:58 +0900

    DAO編集

--- source/src/main/java/dto/Manual.java
+++ source/src/main/java/dto/Manual.java
@@ -27,6 +27,10 @@ public class Manual implements Serializable {
 		this.date = Date.valueOf(date); // String型「YYYY-MM-DD」をSQLのDATE型に入れられるように変換
 	}
 	
+	public Manual() {
+		
+	}
+	
 	public Manual(String manualFile, int importance, String date) {
 		this.manualFile = manualFile;
 		this.importance = importance;
