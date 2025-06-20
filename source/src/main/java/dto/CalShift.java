10dd48acb7b9b41ddffa3da9a985034f317297d1
plusdojo2025-aoki <aoki-sora-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-20 15:20:12 +0900

    カレンダー

---
+++ source/src/main/java/dto/CalShift.java
@@ -0,0 +1,24 @@
+package dto;
+
+import java.sql.Date;
+
+public class CalShift {
+
+	//カレンダーに表示するイベントの日付と件数を格納する
+	private Date shiftData;
+	private int count;
+	
+	public Date getShiftData() {
+		return shiftData;
+	}
+	public void setShiftData(Date shiftData) {
+		this.shiftData = shiftData;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+}
