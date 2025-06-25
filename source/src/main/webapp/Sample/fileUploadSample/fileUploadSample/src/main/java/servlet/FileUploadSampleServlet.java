84998de02d209ee75d66d18ac93bd41675084781
plusdojo2025-futagami <futagami-masanobu-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-25 21:43:00 +0900

    Update FileUploadSampleServlet.java

--- source/src/main/webapp/Sample/fileUploadSample/fileUploadSample/src/main/java/servlet/FileUploadSampleServlet.java
+++ source/src/main/webapp/Sample/fileUploadSample/fileUploadSample/src/main/java/servlet/FileUploadSampleServlet.java
@@ -20,15 +20,15 @@ import javax.servlet.http.Part;
 @WebServlet("/FileUploadSampleServlet")
 public class FileUploadSampleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public FileUploadSampleServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
+	
+	/**
+	* @see HttpServlet#HttpServlet()
+	*/
+	public FileUploadSampleServlet() {
+	super();
+	// TODO Auto-generated constructor stub
+	}
+	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -37,37 +37,34 @@ public class FileUploadSampleServlet extends HttpServlet {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/fileUpload.jsp");
 		dispatcher.forward(request, response);
 	}
-
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-
 		Part part = request.getPart("IMAGE"); // getPartで取得
-
+		
 		String image = this.getFileName(part);
 		request.setAttribute("image", image);
 		// サーバの指定のファイルパスへファイルを保存
-        //場所はクラス名↑の上に指定してある
+		//場所はクラス名↑の上に指定してある
 		part.write(image);
-        //ディスパッチ
+		//ディスパッチ
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
-		dispatcher.forward(request, response);
-    }
-
+		dispatcher.forward(request, response);s
+	}
+	
 	//ファイルの名前を取得してくる
 	private String getFileName(Part part) {
-        String name = null;
-        for (String dispotion : part.getHeader("Content-Disposition").split(";")) {
-            if (dispotion.trim().startsWith("filename")) {
-                name = dispotion.substring(dispotion.indexOf("=") + 1).replace("\"", "").trim();
-                name = name.substring(name.lastIndexOf("\\") + 1);
-                break;
-            }
-        }		// TODO 自動生成されたメソッド・スタブ
+		String name = null;
+		for (String dispotion : part.getHeader("Content-Disposition").split(";")) {
+			if (dispotion.trim().startsWith("filename")) {
+				name = dispotion.substring(dispotion.indexOf("=") + 1).replace("\"", "").trim();
+				name = name.substring(name.lastIndexOf("\\") + 1);
+				break;
+			}
+		}
 		return name;
 	}
-
-
 }
